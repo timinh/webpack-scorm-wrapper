d657275599958133fa7b2f38ce8bcc2d206c3fa6
girobin <gilles.robin@uca.fr>
2020-03-02 17:06:33 +0100

    Separation méthode debugage

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import Scorm2004to12Wrapper from './Scorm2004to12Wrapper'
 
-window.API_1484_11 = new Scorm2004to12Wrapper(false)
+window.API_1484_11 = new Scorm2004to12Wrapper()
+window.API_1484_11.setDebug(true);
